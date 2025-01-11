f30fd9a0aa04224c91f402e205f8d17023732303
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-01-12 01:53:41 +0700

    Refactor: missing changes port_scanner -> dart_port_scanner

--- test/port_scanner_test.dart
+++ test/port_scanner_test.dart
@@ -1,6 +1,6 @@
 import 'package:dart_port_scanner/src/domain/entities/report.dart';
 import 'package:test/test.dart';
-import 'package:dart_port_scanner/port_scanner.dart';
+import 'package:dart_port_scanner/dart_port_scanner.dart';
 
 void main() {
   group('Report tests', () {
